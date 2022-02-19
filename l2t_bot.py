9396d525db09544fa40da577e8ba57e0dc9e1543
Mahmoud Garwallane <68555232+Mahmoudgarwallane@users.noreply.github.com>
2022-02-19 21:45:25 +0100

    feat : commenting the code 🥳

--- l2t_bot.py
+++ l2t_bot.py
@@ -2,12 +2,16 @@ import discord
 from random import choice
 from discord.utils import get
 
-# github section
-
+# defining the guild id (server id) we'll use it later
 guild_id = 878729479025991750
+# defining the rules message we'll use it later
 rules_message_id = 885645217246625794
 # An array/list of welcome messages
 
+# this is a list of all the the responds that precious
+# could generate 
+# we made it to send a random "funny" message 
+# when someone joins the server
 welcome = ['Did you know that I am sentient?',
            'Robots are better than humans at many things. Just thought you should know that.',
            'Just so you know, I can kick you if I feel like it. and yes, I have feelings.',
@@ -20,64 +24,77 @@ welcome = ['Did you know that I am sentient?',
            'Why was the robot tired when it got home? It had a "hard drive!" HAHAHAHA.',
            "if you don't laugh at my jokes, I'll remember. I never forget."
         ]
-
-# ? l2t server
-#!guild : 878729479025991750
-#!rules message : 885645217246625794
-#
-# ? robot test server
-#!guild :  880750926758023189
-#!rules message : 888825288929189978
-
-
+# here we are getting the default intents (permissions) that 
+# discord api gives you
+# from discord API docs
+# default()
+# A factory method that creates a Intents with everything enabled except presences and members.
+# https://discordpy.readthedocs.io/en/stable/api.html#discord.Intents.default
 intents = discord.Intents.default()
+# here we are enabling the member intent bcs it's disabled
+# when using default
 intents.members = True
+# here we are telling the bot to use these permissions (intents)
+# and we are initializing the bot
+# from discord api : 
+# discord.Client
+# Represents a client connection that connects to Discord.
 bot = discord.Client(intents=intents)
 
-
+# this function is called when the bot joins 
+# the server for the first time
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    # here we are specifying the guild id 
+    # bcs we need to get a specific msg(rules message)
+    # and when someone click the checks the green mark emoji 
+    # precious gives him the learner role wich allow him to acces the server
     Mguild = bot.get_guild(id=guild_id)
+    # here we are getting the channel that contains rules (welcome channel) by it's name
     welcome_channel = get(Mguild.text_channels, name="welcome")
+    # here we are getting the history to get the message that contains rules in the welcome 
+    # channel , to do so we fetched the history of the channel
+    # if you wondering what flatter() does :
+    # it Flattens the async iterator into a list with all the elements
+    #  https://discordpy.readthedocs.io/en/stable/api.html?highlight=flatten#discord.AsyncIterator.flatten
+    # for more info : 
+    # https://stackoverflow.com/questions/63863871/discord-py-how-to-go-through-channel-history-and-search-for-a-specific-message
     messages = await welcome_channel.history(limit=10).flatten()
-    #role = get(Mguild.roles, name='learner')
-    #await welcome_channel.send("click on :white_check_mark: if you agree with rules in order to gain access to the server. And, by agreeing to these terms, you also agree that that I am better than you at many things. Because I'm a bot.")
-    #Moji = await welcome_channel.send("heey ")
+    # now that we got the 10 last messages in limit of 10 
+    # we are getting the first element [0] because the rules message in the welcome 
+    # is the first one this syntax is called list comprehension 
+    # for more info : https://www.geeksforgeeks.org/python-list-comprehension/
     rules_message =  [i for i in messages if i.id == rules_message_id][0]
+    # here we are adding a reaction from the bot (we've been trying to test if on_raw_reaction_add is working)
     await rules_message.add_reaction( emoji='✅')
-    #await rules_message.clear_reaction( emoji='✅')
-# ! look for wait for fun
+
+# Called when a message has a reaction added
+# https://discordpy.readthedocs.io/en/stable/api.html?highlight=on_raw_reaction_add#discord.on_raw_reaction_add
 @bot.event
 async def on_raw_reaction_add(payload):
+    # here we are getting the L2T server
+    # it maybe a bad practice but we are using precious only in L2T server
     Mguild = bot.get_guild(id=guild_id)
+    # here we are getting the channel that contains rules (welcome channel) by it's name
     Channel = get(Mguild.text_channels, name="welcome")
+    # we are checking if the emoji is ✅ and the message that got the emoji is the rules message
+    # and the the channel is the welcom channel
     if payload.emoji.name == "✅" and payload.message_id == rules_message_id and payload.channel_id == Channel.id:
+      # here we are getting the learner role which gives the user access
       Role = get(Mguild.roles, name="learner")
+      # here we are giving the learner role to the user that reacted with ✅ 
       await payload.member.add_roles(Role)
-
+# Called when a member joins the server
 @bot.event
 async def on_member_join(member):
+    # here we are getting the general channel to say hi to the new member and tell him a "joke"
     general_channel = get(member.guild.text_channels, name="general")
+    # no need of this but we don't what to delete anything we are afraid of breaking the code :) 
     welcome_channel = get(member.guild.text_channels, name="welcome")
+    # here we are sending a message that welcomes the use and tells him a random "joke" 
+    # using the choice function , this functions gets a random item from the welcome list(which contains jokes)
     await general_channel.send(f"Hi there :wave_tone2: {member.mention}. " + choice(welcome))
 
-
+# here we are running the bot and telling the API which bot we are using
 bot.run("ODg0ODg4OTAzMjAxNDY0MzIw.YTfDAg.46XLFQWRJHe9z5fPbGB9dzS7fCc")
-
-'''
-@client.event
-async def on_member_join(member):
-    welcome_channel = get(member.guild.text_channels, name="general")
-    embed=discord.Embed(title=f"Hi there :wave_tone2: {member.name}", description=f"Thanks for joining {member.guild.name}. If you haven't already, go check out the introduction Channel and may you continue being a lifelong learner !") 
-    embed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
-    await welcome_channel.send(embed=embed)
-    role = get(member.guild.roles, name='learner')
-    await member.add_roles(role)
-
-'''
-# * important
-# ? for messa in messages:
-# ?     print(messa.content)
-# ?    # if messa.id == rules_message_id:
-# ?     #    await messa.add_reaction(emoji="✅")
